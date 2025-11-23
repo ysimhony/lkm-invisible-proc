7dd243eeabbad5f6265dba5f0910e859bb4516eb
conguyen <conguyen@axon.com>
2025-11-23 16:45:33 +0700

    Add invisible proc example.

--- Makefile
+++ Makefile
@@ -1,15 +1,20 @@
-obj-m := vm.o
+obj-m := lkm.o
 KERNEL_DIR := /home/conguyen/repos/personal/linux
 CROSS_COMPILE := /home/conguyen/repos/personal/arm64-toolchain/bin/aarch64-none-linux-gnu-
+GCC := $(CROSS_COMPILE)gcc
 ROOTFS_PATH := $(PWD)/rootfs
 ARCH := arm64
 
-all:
+all: usr
 	make -C $(KERNEL_DIR) M=$(PWD) ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_COMPILE) modules
 
 clean:
 	make -C $(KERNEL_DIR) M=$(PWD) ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_COMPILE) clean
 
+usr:
+	$(GCC) -o invisible_proc invisible_proc.c -static
+
 install:
-	cp $(PWD)/vm.ko $(ROOTFS_PATH)/
+	cp $(PWD)/invisible_proc $(ROOTFS_PATH)/
+	cp $(PWD)/lkm.ko $(ROOTFS_PATH)/
 	cd $(ROOTFS_PATH) && find . -print0 | cpio --null -ov --format=newc | gzip -9 > $(PWD)/rootfs.cpio.gz && cd -
---
+++ invisible_proc.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <time.h>
+
+#define PORT 3000
+#define SERVER_IP "10.0.2.2"
+#define MESSAGE "Invisible process is alive"
+
+int main() {
+    int sockfd;
+    struct sockaddr_in servaddr = {0};
+    char message[100] = {0};
+    time_t now;
+    int pid = getpid();
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        return EXIT_FAILURE;
+    }
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, SERVER_IP, &servaddr.sin_addr) <= 0) {
+        perror("Invalid server IP address");
+        close(sockfd);
+        return EXIT_FAILURE;
+    }
+
+    int count = 0;
+    while (1) {
+        count++;
+        now = time(NULL);
+        memset(message, 0, sizeof(message));
+        sprintf(message, "[%d][%d] " MESSAGE ": %d\n", now, pid, count);
+
+        if (sendto(sockfd, (const char *)message, strlen(message),
+                0, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+            perror("sendto failed");
+            close(sockfd);
+            return EXIT_FAILURE;
+        }
+
+        sleep(5);
+    }
+
+    close(sockfd);
+    return EXIT_SUCCESS;
+}
